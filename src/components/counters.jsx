7866c2d4124396d126f7e8b433ad8dc934cfb407
Mehran-rzi <mehranrezaea@gmail.com>
2024-01-08 16:30:52 +0330

    add reset buttom and use single source of truth with removing the local state (and change all references)

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -10,17 +10,35 @@ class Counters extends Component {
         { id:4, value:0}
     ]
      };
-     handlerDelete = counterId=>{
+     handleIncrement = counter=>{
+        const counters = [...this.state.counters];
+        const index = counters.indexOf(counter);
+        counters[index] ={...counter};
+        counters[index].value++;
+        this.setState({counters});
+     };
+     handleReset = ()=>{
+        const counters = this.state.counters.map(c=> {
+            c.value =0; 
+            return c;
+        });
+        this.setState({counters});
+     };
+     handleDelete = counterId=>{
         const counters = this.state.counters.filter(c=> c.id !== counterId);
         this.setState({counters});
      };
     render() { 
         return (
         <div>
+            <buttom 
+            onClick={this.handleReset}
+            className="btn btn-primary btn-sm m-2">Reset</buttom>
             { this.state.counters.map(counter =>(
                  <Counter
                   key={counter.id}
-                  onDelete={this.handlerDelete} 
+                  onDelete={this.handleDelete} 
+                  onIncrement={this.handleIncrement}
                   counter={counter}/>
             
                  ))}
