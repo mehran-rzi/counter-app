bc0b8726ae981e24f45426029916631e875b87e0
Mehran-rzi <mehranrezaea@gmail.com>
2024-01-08 15:29:11 +0330

    add delete buttom for delete item of list

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -3,18 +3,25 @@ import Counter from "./counter";
 
 class Counters extends Component {
     state = { 
-        counter:[
+        counters:[
         { id:1, value:4},
         { id:2, value:0},
         { id:3, value:0},
         { id:4, value:0}
     ]
-     } 
+     };
+     handlerDelete = counterId=>{
+        const counters = this.state.counters.filter(c=> c.id !== counterId);
+        this.setState({counters});
+     };
     render() { 
         return (
         <div>
-            { this.state.counter.map(counter =>(
-                 <Counter key={counter.id} value={counter.value} id={counter.id}/>
+            { this.state.counters.map(counter =>(
+                 <Counter
+                  key={counter.id}
+                  onDelete={this.handlerDelete} 
+                  counter={counter}/>
             
                  ))}
         </div>
