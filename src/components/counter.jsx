be00b8335e74c5a149086a6d02e95661752ea008
Mehran-rzi <mehranrezaea@gmail.com>
2024-01-09 12:17:20 +0330

    final counter app

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,15 +3,34 @@ import React, { Component } from "react";
 class Counter extends Component {
   render() {
     return (
-      <div>
+      <div className="row">
+        <div className="col-1">
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        </div>
+        <div className="col">
         <button
           onClick={()=>this.props.onIncrement(this.props.counter)}
           className="btn btn-secondary btn-sm"
         >
-          Increment
+          +
         </button>
-        <buttom onClick={()=>this.props.onDelete(this.props.counter.id)} className="btn btn-danger btn-sm m-2">Delete</buttom>
+        <button
+          onClick={()=>this.props.onDecrement(this.props.counter)}
+          className="btn btn-secondary btn-sm m-2"
+          disabled={this.props.counter.value === 0 ? 'disable' : ''}
+        >
+          -
+        </button>
+        <buttom 
+        onClick={()=>this.props.onDelete(this.props.counter.id)} 
+        className="btn btn-danger btn-sm"
+        >
+          Delete
+          </buttom>
+
+        </div>
+        
+        
       </div>
     );
   }
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -3,7 +3,7 @@ import Counter from "./counter";
 
 class Counters extends Component {
     render() { 
-        const {onReset, counters, onDelete, onIncrement} =this.props;
+        const {onReset, counters, onDelete, onIncrement, onDecrement} =this.props;
         return (
         <div>
             <buttom 
@@ -14,6 +14,7 @@ class Counters extends Component {
                   key={counter.id}
                   onDelete={onDelete} 
                   onIncrement={onIncrement}
+                  onDecrement={onDecrement}
                   counter={counter}/>
             
                  ))}
