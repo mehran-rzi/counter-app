803ad8b54e2d921a050b62f7869a62c01bc4ce05
Mehran-rzi <mehranrezaea@gmail.com>
2024-01-09 10:04:34 +0330

    use navbar bar for show how many item in list

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -5,7 +5,9 @@ class NavBar extends Component {
         return (
             <nav className="navbar bg-body-tertiary">
   <div className="container-fluid">
-    <a className="navbar-brand" href="#">Navbar</a>
+    <a className="navbar-brand" href="#">
+      Navbar <span className="badge bg-pill bg-secondary">{this.props.totalCounters}</span>
+    </a>
   </div>
 </nav>
         );
