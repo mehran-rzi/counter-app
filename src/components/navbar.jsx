4d6230baaa8bbfcfd3bf3600a26ad54ab125272b
Mehran-rzi <mehranrezaea@gmail.com>
2024-01-09 10:16:45 +0330

    use stateless functional components instead of class (because we don't need state -> Easier management)

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,17 +1,16 @@
 import React, { Component } from 'react';
 
-class NavBar extends Component {
-    render() { 
-        return (
-            <nav className="navbar bg-body-tertiary">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="#">
-      Navbar <span className="badge bg-pill bg-secondary">{this.props.totalCounters}</span>
-    </a>
-  </div>
+const NavBar = (props) => {
+  return ( 
+      <nav className="navbar bg-body-tertiary">
+<div className="container-fluid">
+  <a className="navbar-brand" href="#">
+      Navbar <span className="badge bg-pill bg-secondary">{props.totalCounters}</span>
+      </a>
+</div>
 </nav>
-        );
-    }
+      
+   );
 }
- 
+
 export default NavBar;
