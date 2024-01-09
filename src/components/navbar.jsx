4e17c0a587167f991d17b09c8276289c1ddc6aa4
Mehran-rzi <mehranrezaea@gmail.com>
2024-01-09 11:07:50 +0330

    Simplify with Destructuring Arguments

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 
-const NavBar = (props) => {
+const NavBar = ({totalCounters}) => {
   return ( 
       <nav className="navbar bg-body-tertiary">
 <div className="container-fluid">
   <a className="navbar-brand" href="#">
-      Navbar <span className="badge bg-pill bg-secondary">{props.totalCounters}</span>
+      Navbar <span className="badge bg-pill bg-secondary">{totalCounters}</span>
       </a>
 </div>
 </nav>
