88db1b697918988cbf888e1a3a542ef82ae95951
Mehran-rzi <mehranrezaea@gmail.com>
2024-01-09 09:37:01 +0330

    some change for add navbar

--- src/App.js
+++ src/App.js
@@ -1,31 +1,17 @@
-import React, { useState } from "react";
-import "./app.css";
+import React, { Component } from 'react';
+import './App.css';
+import NavBar from './components/navbar';
+import Counters from "./components/counters";
 
-export default function App() {
-  const [count, setCount] = useState(0); // useState returns a pair. 'count' is the current state. 'setCount' is a function we can use to update the state.
-
-  function increment() {
-    //setCount(prevCount => prevCount+=1);
-    setCount(function (prevCount) {
-      return (prevCount += 1);
-    });
-  }
-
-  function decrement() {
-    setCount(function (prevCount) {
-      if (prevCount > 0) {
-        return (prevCount -= 1); 
-      } else {
-        return (prevCount = 0);
-      }
-    });
-  }
-
+function App() {
   return (
-    <div className="App">
-      <h1>{count}</h1>
-      <button onClick={increment}>Increment</button>
-      <button onClick={decrement}>Decrement</button>
-    </div>
+    <React.Fragment>
+      <NavBar />
+    <main className='container'>
+      <Counters />
+    </main>
+    </React.Fragment>
   );
 }
+
+export default App;
